0d4b7d2ba17bf4c0f878bc32d0ee3cc7951988d6
Kenneth Keller <kenkanifffromct21@gmail.com>
2021-03-01 20:52:44 -0500

    adding schema and seeds files

---
+++ DB/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Quarter Pounder', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mondo Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Good Burger', false);
