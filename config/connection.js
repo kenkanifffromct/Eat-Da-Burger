a5f5337df15cfdd49ee274d971cc45f675d79b5e
Kenneth Keller <kenkanifffromct21@gmail.com>
2021-03-03 18:35:20 -0500

    updated server and connection JS files

---
+++ config/connection.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const mysql = require("mysql");
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "127.0.0.1",
+    port: 3306,
+    user: "root",
+    password: process.env.DB_PASSWORD,
+    database: "burger_db"
+  });
+}
+
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
