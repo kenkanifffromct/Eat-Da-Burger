9cf57f7919f0faf8f98ce3629cdda24942f59504
Kenneth Keller <kenkanifffromct21@gmail.com>
2021-03-03 21:26:22 -0500

    updating connection.js

--- config/connection.js
+++ config/connection.js
@@ -6,10 +6,10 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-    host: "127.0.0.1",
+    host: "localhost",
     port: 3306,
     user: "root",
-    password: process.env.DB_PASSWORD,
+    password: "Mydogsk1p!",
     database: "burger_db"
   });
 }
